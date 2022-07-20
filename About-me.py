136ed94ce1687c98f83001aac8138ddb1ac575b0
habib <habibalizadeh4570@gmail.com>
2022-07-20 04:17:27 -0400

    New changes made

--- About-me.py
+++ About-me.py
@@ -1,1 +1,9 @@
-print('hello AI-Hexa')
+from posixpath import split
+from colorama import Fore 
+print(Fore.BLUE + 'Processed...')
+import numpy as np 
+import pandas as pd
+
+question = input('Ask me a question :\n')
+
+split_question = question.split(sep = ' ')
