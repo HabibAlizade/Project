ebfb67b1a3a320651d5a1ac649fcbd7a0f9fceab
Habib Alizadeh <35423193+HabibAlizadeh45@users.noreply.github.com>
2022-07-24 14:27:18 -0400

    deleted

--- main.py
+++
@@ -1,8 +0,0 @@
-import time
-def dprint(str):
-    for c in str:
-        print(c, end = '', flush = True)
-        time.sleep(0.08)
-
-dprint('Please upload your data into the folder and write the name of the file here :')
-
