0e256aba410d8ec898263a671c7c90d4fbf9ae12
Habib Alizadeh <35423193+HabibAlizadeh45@users.noreply.github.com>
2022-07-24 11:21:15 -0400

    delay print was added

--- About-me.py
+++ About-me.py
@@ -1,3 +1,8 @@
-question = input('Ask me a question :\n')
-print(f'your question is {question}')
+import time
+def dprint(str):
+    for c in str:
+        print(c, end = '', flush = True)
+        time.sleep(0.08)
+
+dprint('Please upload your data into the folder and write the name of the file here :')
 
